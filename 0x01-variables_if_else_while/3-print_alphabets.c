acf4ded6a309a7a6556c1a25dff0b56da09309ff
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2022-12-10 06:02:55 -0800

    Print alphabet (lower and upper)

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x = 'a';
+
+	char y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
