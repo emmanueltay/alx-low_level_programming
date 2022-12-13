6408e532f6135684d4074937f15b73e25d208409
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2022-12-13 13:44:37 -0800

    Ready to commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,19 @@
 int print_sign(int n)
 {
 	if (n > 0)
+	{
 		return (1);
 		_putchar('+');
-	if (n == 0)
+	}
+	else if (n == 0)
+	{
 		return (0);
 		_putchar('0');
-	if (n < 0)
+	}
+	else
+	{
 		return (-1);
 		_putchar('-');
+	}
 	return (0);
 }
