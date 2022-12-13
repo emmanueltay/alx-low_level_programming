61951f8a3705426884ce771dd424f4938d25e8b0
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2022-12-13 14:03:16 -0800

    Ready to commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - it returns the absolute value of an integer passed to it
+ * @x: is the integer passed to the function
+ * Return: The absolute value
+ */
+
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return (-x);
+}
