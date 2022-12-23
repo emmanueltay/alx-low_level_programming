00d559a71ac6e442ba7c285dab0ee53baf92b70c
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2022-12-22 21:51:27 -0800

    Happy coding

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @str: pointer to string
+ * Return: string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == data1[j])
+			{
+				str[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
