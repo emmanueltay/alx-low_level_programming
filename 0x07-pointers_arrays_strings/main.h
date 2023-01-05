3bcc6d66afa3c7907c5a2fc86e5915effa668d16
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2023-01-04 20:32:42 -0800

    happy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,6 +10,6 @@ char *_strchr(char *s, char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
-
+void set_string(char **s, char *to)
 
 #endif
