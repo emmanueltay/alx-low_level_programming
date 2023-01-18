9f358717fa05a6e922bb00fae531578c1d6494e6
Adzi-Tay Emmanuel <adzitayemmanuel@gmail.com>
2023-01-17 19:54:29 -0800

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,10 @@ struct dog
 };
 
 /**
- * dog_t - ypedef for struct dog
+ * dog_t - typedef for struct dog
  */
 
-typedef dog dog_t;
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
